b6cfefad115c904cedea7b423c103e616e2d87a2
Marchjul <marchellogiovani28@gmail.com>
2026-04-07 15:39:21 +0700

    struct alamat dan mhs

---
+++ StructGetline.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct alamatDetail
+{
+    string desa;
+    string kota;
+
+};
+
+struct mahasiswa 
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
