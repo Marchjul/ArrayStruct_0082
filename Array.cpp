7d97e3a166964c9da87668b8d3d29b59918b6d5f
Marchjul <marchellogiovani28@gmail.com>
2026-04-07 15:03:08 +0700

    operasi matematika

--- Array.cpp
+++ Array.cpp
@@ -5,5 +5,16 @@ int main(){
     //deklarasi array dengan inisialisasi
     int dataku[5] = {10,20,30,40,50};
     //deklarasi tanpa inisialisasi
-    
+    int datamu[5];
+    int x;
+    //cara membaca array dataku
+    cout << "Data ke - 3 : " << dataku[2]<<endl;
+
+    //operasi matematika
+    x = dataku[2]*3;
+    //cara menulis atau mengisi array
+    dataku[2] = 600;
+    cout << "Data ke - 3 yang baru : " << dataku[2]<<endl;
+
+
 }
