993d213b9ed2103a4c750b9948991ce9a19c3b0f
Marchjul <marchellogiovani28@gmail.com>
2026-04-07 15:22:46 +0700

    pengulangan for

--- Array.cpp
+++ Array.cpp
@@ -28,5 +28,33 @@ int main(){
     cout << "Data index ke - 4 : " ;
     cin >> datamu[4];
 
+    cout << endl;
+
+    //tampilkan array datamu satu persatu
+    cout << "Data ke - 1 : " << datamu[0]<<endl;
+    cout << "Data ke - 2 : " << datamu[1]<<endl;
+    cout << "Data ke - 3 : " << datamu[2]<<endl;
+    cout << "Data ke - 4 : " << datamu[3]<<endl;
+    cout << "Data ke - 5 : " << datamu[4]<<endl;
+
+    cout << endl;
+
+    //isi data dengan pengulangan for
+    for (int i = 0; i <= 4; i++)
+    {
+
+        cout << "Data Index ke - "<< i <<" : " ;
+        cin >> datamu[i];
+
+    }
+
+    cout<<endl;
+    //menampilkan datamu dengan pengulangan for
+    for (int i = 0; i<= 4; i++)
+    {
+        cout << "Data ke - "<< i+1 << " : "<< datamu[i]<<endl;
+
+    }
+
 
 }
