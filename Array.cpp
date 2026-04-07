fd9c4345a0e63200a76f26e3a23f7261337014be
Marchjul <marchellogiovani28@gmail.com>
2026-04-07 14:50:30 +0700

    deklarasi array

---
+++ Array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //deklarasi array dengan inisialisasi
+    int dataku[5] = {10,20,30,40,50};
+    //deklarasi tanpa inisialisasi
+    
+}
